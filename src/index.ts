c422f4dcea2fc594438bfe5d75f5fcc1c71a1432
Ethan <yishionwang314@163.com>
2026-08-04 23:09:39 +0800

    Export public library API

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { loadClipMap, resolveClipRule } from './config.js';
+export { applyClipsToMeta, resolveMetaPath, updateMetaFile } from './meta.js';
+export { parseClipsFromFilename } from './parser.js';
+export * from './types.js';
